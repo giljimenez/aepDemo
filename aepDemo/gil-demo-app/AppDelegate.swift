6bebc8794c208289e52b901162ba0437d517a2dd
Gilbert Jimenez <giljimenez@gmail.com>
2020-09-16 14:41:08 -0500

    added initial pods including core, profile, and analytics. initialized lifecycle calls and set to verbose reporting for AEP SDK

--- aepDemo/gil-demo-app/AppDelegate.swift
+++ aepDemo/gil-demo-app/AppDelegate.swift
@@ -1,4 +1,7 @@
 import UIKit
+import ACPCore
+import ACPAnalytics
+import ACPUserProfile
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,6 +9,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        ACPCore.setLogLevel(.verbose)
+        ACPCore.configure(withAppId: "db150ae210e9/9de8fbbe8941/launch-3b8201ffa5b2-development")
+        ACPIdentity.registerExtension()
+        ACPLifecycle.registerExtension()
+        ACPSignal.registerExtension()
+        ACPUserProfile.registerExtension()
+        ACPCore.start {
+            ACPCore.lifecycleStart(nil)
+        }
         return true
     }
 
